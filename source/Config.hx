10aca03f63d0455087124d4971ac72eab54cb1a1
aflac <60161366+aflacc@users.noreply.github.com>
2021-03-30 16:13:34 -0700

    Testing Testing 123

---
+++ source/Config.hx
@@ -0,0 +1,4 @@
+// This is the config folder for ProjectFNF
+// These are config options the player doesn't get to access so you can keep the game looking as intended.
+public var CONFIGGfCar:Bool = true; // If you want GF on week 4 to be sitting on the car in the back and not on a speaker.
+public var CONFIGTitle:Bool = true; // Title screen uses ProjectFNF logo instead of the Logo bumpin.
