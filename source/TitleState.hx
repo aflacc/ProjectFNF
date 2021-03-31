88ec91566c42cb2cf6c3afc04d70e36b2a2d3600
aflac <60161366+aflacc@users.noreply.github.com>
2021-03-31 12:09:44 -0700

    frick

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -192,7 +192,7 @@ class TitleState extends MusicBeatState
 		blackScreen = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		credGroup.add(blackScreen);
 
-		credTextShit = new Alphabet(0, 0, "ninjamuffin99\nPhantomArcade\nkawaisprite\nevilsk8er", true);
+		credTextShit = new Alphabet(0, 0, "ninjamuffin99\nPhantomArcade\nkawaisprite\nevilsk8er\naflac", true);
 		credTextShit.screenCenter();
 
 		// credTextShit.alignment = CENTER;
