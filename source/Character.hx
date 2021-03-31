d94579ae4f1bedcba1b3aa2b992230232a7683ab
aflac <60161366+aflacc@users.noreply.github.com>
2021-03-30 21:22:43 -0700

    amogus :D

--- source/Character.hx
+++ source/Character.hx
@@ -296,6 +296,7 @@ class Character extends FlxSprite
 				animation.addByPrefix('firstDeath', "BF dies", 24, false);
 				animation.addByPrefix('deathLoop', "BF Dead Loop", 24, true);
 				animation.addByPrefix('deathConfirm', "BF Dead confirm", 24, false);
+				animation.addByPrefix('hit', "hit", 24, false);
 
 				animation.addByPrefix('scared', 'BF idle shaking', 24);
 
@@ -313,6 +314,7 @@ class Character extends FlxSprite
 				addOffset('deathLoop', 37, 5);
 				addOffset('deathConfirm', 37, 69);
 				addOffset('scared', -4);
+				addOffset('hit', -1, 10);
 
 				playAnim('idle');
 
@@ -358,6 +360,7 @@ class Character extends FlxSprite
 				animation.addByPrefix('singLEFTmiss', 'BF NOTE LEFT MISS', 24, false);
 				animation.addByPrefix('singRIGHTmiss', 'BF NOTE RIGHT MISS', 24, false);
 				animation.addByPrefix('singDOWNmiss', 'BF NOTE DOWN MISS', 24, false);
+				animation.addByPrefix('hit', "hit", 24, false);
 
 				addOffset('idle', -5);
 				addOffset("singUP", -29, 27);
@@ -368,6 +371,7 @@ class Character extends FlxSprite
 				addOffset("singRIGHTmiss", -30, 21);
 				addOffset("singLEFTmiss", 12, 24);
 				addOffset("singDOWNmiss", -11, -19);
+				addOffset('hit', -1, 10);
 				playAnim('idle');
 
 				flipX = true;
