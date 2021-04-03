f16d27206adbe7ec2e165c2032d8341aff9c7ae2
aflac <60161366+aflacc@users.noreply.github.com>
2021-04-03 13:08:16 -0700

    send pie

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -26,9 +26,9 @@ class HealthIcon extends FlxSprite
 		animation.add('tankman', [8, 9], 0, false, isPlayer);
 		animation.add('face', [10, 11], 0, false, isPlayer);
 		animation.add('dad', [12, 13], 0, false, isPlayer);
-		animation.add('senpai', [22, 22], 0, false, isPlayer);
-		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
-		animation.add('spirit', [23, 23], 0, false, isPlayer);
+		animation.add('senpai', [22, 22], 0, false, isPlayer); // I personally hate icon 23
+		animation.add('senpai-angry', [24, 25], 0, false, isPlayer);
+		animation.add('spirit', [26, 27], 0, false, isPlayer);
 		animation.add('bf-old', [14, 15], 0, false, isPlayer);
 		animation.add('gf', [16], 0, false, isPlayer);
 		animation.add('parents-christmas', [17], 0, false, isPlayer);
