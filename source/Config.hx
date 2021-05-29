d8c605bf7d00a3878191a9541acd24049debc31d
aflac <60161366+aflacc@users.noreply.github.com>
2021-05-29 16:14:28 -0700

    Proper Config Options 2x

--- source/Config.hx
+++ source/Config.hx
@@ -1,7 +1,8 @@
-// This is the config folder for ProjectFNF
-// These are config options the player doesn't get to access so you can keep the game looking as intended.
-
 /**
+ *  This is the config folder for ProjectFNF
+ *
+ * These are config options the player doesn't get to access so you can keep the game looking as intended.
+ * 
  * config options lmao
  */
 class Config extends MusicBeatState
@@ -9,31 +10,43 @@ class Config extends MusicBeatState
 	/**
 	 * Makes the Girlfriend sit on the Limo instead of the Speaker in week 4.
 	 * Its very funny
+	 * 
+	 * Default = **true**
 	 */
 	public static var CONFIGGfCar:Bool = true;
 
 	/**
 	 * Use the ProjectFNF logo or the Friday Night Funkin logo??
+	 * 
+	 * Default = **true**
 	 */
 	public static var CONFIGTitle:Bool = true;
 
 	/**
 	 * Skips the "You are using a bla bla bla projectfnf bla bla bla github bla bla bla" message
+	 * 
+	 * Default = **false**
 	 */
 	public static var CONFIGSkip:Bool = true;
 
 	/**
 	 *  The Name of the Mod. This shows up in the Main Menu with the Story Mode, Freeplay, Donate, and Options buttons
+	 * 
+	 * Default = **ProjectFNF**
 	 */
 	public static var ModName:String = 'ProjectFNF';
 
 	/**
 	 * This is a WIP thing. I don't fully know what I'm gonna do with this but I might just have it make the Output be a little more in depth.
+	 * 
+	 * Default = **false**
 	 */
 	public static var DEBUGMODE:Bool = false;
 
 	/**
 	 * Shakes the camera when you miss a note. Shakes on the Axis you missed the note of. (Up/Down will shake the camera Up and Down)
+	 * 
+	 * Default = **true**
 	 */
 	public static var MISSFX:Bool = true;
 
@@ -43,6 +56,8 @@ class Config extends MusicBeatState
 	 * It is not reccomended to set this *too* high, it gets very obnoxious ***VERY*** quickly.
 	 * 
 	 * Suggested to use any value between .001 - .01
+	 * 
+	 * Default = **0.002**
 	 */
 	public static var MISSINTENSITY = 0.002;
 }
