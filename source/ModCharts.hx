89de259945b874e7755e1753b377ed8d063f0d50
aflac <60161366+aflacc@users.noreply.github.com>
2021-05-28 20:55:29 -0700

    push out update

--- source/ModCharts.hx
+++ source/ModCharts.hx
@@ -4,11 +4,11 @@ import flixel.tweens.FlxTween;
 
 class ModCharts
 {
-    static public var stickNotes:Bool = true;
-    static public var dadNotesVisible:Bool = true;
-    static public var bfNotesVisible:Bool = true;
+	static public var stickNotes:Bool = true;
+	static public var dadNotesVisible:Bool = true;
+	static public var bfNotesVisible:Bool = true;
 
-    /**
+	/**
 	 * Quickly spin a sprite 180 degrees. Usually used for spinning the strum arrows.
 	 *
 	 * ```haxe
@@ -17,11 +17,17 @@ class ModCharts
 	 *
 	 * @param	Object 		The object to move (FlxObject)
 	 */
-    static public function quickSpin(sprite) {
-        FlxTween.angle(sprite, 0, 360, 0.5, { type: FlxTween.ONESHOT, ease: FlxEase.quadInOut, startDelay: 0, loopDelay: 0 });
-    }
+	static public function quickSpin(sprite)
+	{
+		FlxTween.angle(sprite, 0, 360, 0.5, {
+			type: FlxTween.ONESHOT,
+			ease: FlxEase.quadInOut,
+			startDelay: 0,
+			loopDelay: 0
+		});
+	}
 
-    /**
+	/**
 	 * Loops a sprite in a circle infinetly. DOES NOT ROTATE THE SPRITE!
 	 *
 	 * ```haxe
@@ -31,10 +37,12 @@ class ModCharts
 	 * @param	Object 		The object to move (FlxObject)
 	 * @param	RotateRadius 		The rotating radius of the object
 	 */
-    static public function circleLoop(sprite, rotateradius, speed) {
-        FlxTween.circularMotion(sprite, sprite.x, sprite.y, rotateradius, 90, true, speed, {type: FlxTween.LOOPING});
-    }
-    /**
+	static public function circleLoop(sprite, rotateradius, speed)
+	{
+		FlxTween.circularMotion(sprite, sprite.x, sprite.y, rotateradius, 90, true, speed, {type: FlxTween.LOOPING});
+	}
+
+	/**
 	 * Moves the sprite to a certain place on the screen over time.
 	 *
 	 * ```haxe
@@ -46,10 +54,12 @@ class ModCharts
 	 * @param	y 		The y value of the final destination
 	 * @param	duration 		How long it takes to get there(seconds)
 	 */
-    static public function moveTo(sprite, x, y, duration) {
-        FlxTween.linearMotion(sprite, sprite.x, sprite.y, x, y, duration, true, { type: FlxTween.ONESHOT, ease: FlxEase.quadInOut});
-    }
-    /**
+	static public function moveTo(sprite, x, y, duration)
+	{
+		FlxTween.linearMotion(sprite, sprite.x, sprite.y, x, y, duration, true, {type: FlxTween.ONESHOT, ease: FlxEase.quadInOut});
+	}
+
+	/**
 	 * Bounces the sprite up and down infinenty(WIP, STILL SHAKY)
 	 *
 	 * ```haxe
@@ -59,12 +69,13 @@ class ModCharts
 	 * @param	Object 		The object to bounce (FlxObject)
 	 * @param	duration		How logn it takes to bounce up. Repeats for down
 	 */
-    static public function bounceLoop(sprite, duration) {
-        FlxTween.linearMotion(sprite, sprite.x, sprite.y, sprite.x, sprite.y - 50, duration, true, { type: FlxTween.PINGPONG});
-    }
+	static public function bounceLoop(sprite, duration)
+	{
+		FlxTween.linearMotion(sprite, sprite.x, sprite.y, sprite.x, sprite.y - 50, duration, true, {type: FlxTween.PINGPONG});
+	}
 
-    /**
-	 * Adds a trail to the sprite, like the one spirit has.
+	/**
+	 * Adds a trail to the sprite, like the one spirit has. Pretty much a crossfade effect.
 	 *
 	 * ```haxe
 	 * ModCharts.addTrailToSprite(dad);
@@ -73,10 +84,11 @@ class ModCharts
 	 * @param	Object 		The object to add the effect to (FlxObject)
 	 * @return The trail sprite. simply add it to the project with add(spriteName)
 	 */
-    static public function addTrailToSprite(sprite) {
-        var trail = new FlxTrail(sprite, null, 4, 2, 0.1, 0.069);
-        // evilTrail.changeValuesEnabled(false, false, false, false);
-        // evilTrail.changeGraphic()
-        return trail; // to be added
-    }
+	static public function addTrailToSprite(sprite)
+	{
+		var trail = new FlxTrail(sprite, null, 4, 2, 0.1, 0.069);
+		// evilTrail.changeValuesEnabled(false, false, false, false);
+		// evilTrail.changeGraphic()
+		return trail; // to be added
+	}
 }
