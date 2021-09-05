1e27c5938a614f7f4d8932e9afe6c7489810b803
Stilic <63605602+Stilic@users.noreply.github.com>
2021-09-05 14:12:15 +0200

    Fix story menu characters offsets

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -418,23 +418,28 @@ class StoryMenuState extends MusicBeatState
 		switch (grpWeekCharacters.members[0].animation.curAnim.name)
 		{
 			case 'parents-christmas':
-				grpWeekCharacters.members[0].offset.set(200, 200);
-				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 0.99));
+				grpWeekCharacters.members[0].offset.set(250, 100);
+				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1.5));
 
 			case 'senpai':
-				grpWeekCharacters.members[0].offset.set(130, 0);
+				grpWeekCharacters.members[0].offset.set(130, -25);
 				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1.4));
 
 			case 'mom':
-				grpWeekCharacters.members[0].offset.set(100, 200);
-				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1));
+				grpWeekCharacters.members[0].offset.set(135, 110);
+				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 0.8));
 
 			case 'dad':
-				grpWeekCharacters.members[0].offset.set(120, 200);
+				grpWeekCharacters.members[0].offset.set(160, 110);
 				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1));
 
+			case 'pico':
+				grpWeekCharacters.members[0].offset.set(160, 40);
+				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1));
+				// grpWeekCharacters.members[0].updateHitbox();
+
 			default:
-				grpWeekCharacters.members[0].offset.set(100, 100);
+				grpWeekCharacters.members[0].offset.set(170, 70);
 				grpWeekCharacters.members[0].setGraphicSize(Std.int(grpWeekCharacters.members[0].width * 1));
 				// grpWeekCharacters.members[0].updateHitbox();
 		}
